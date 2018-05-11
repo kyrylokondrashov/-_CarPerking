058cb28969434f2220fa8efe7952bfb498460b9b
Kirill <makfa78@gmail.com>
2018-05-11 22:50:31 +0300

    Add Transaction class. First time didn't push

---
+++ BSA_homework/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSA_homework
+{
+    class Transaction
+    {
+        public DateTime tranactionDate { get; }
+        public uint curId { get; }
+
+        public float moneyRemobeBalance { get; }
+
+        public Transaction (uint id, float money, DateTime date)
+        {
+            curId = id;
+            moneyRemobeBalance = money;
+            tranactionDate = date;
+        }
+
+    }
+}
