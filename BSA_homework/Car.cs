8c44ee4682f3b6d7c4e51cf8f26f32a27ef08ed2
Kirill <makfa78@gmail.com>
2018-05-11 22:39:04 +0300

    Add Car class and enum Car

---
+++ BSA_homework/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSA_homework
+{
+    enum carType
+    {
+        passanger,
+        truck,
+        bus,
+        motorcycle
+    }
+    class Car
+    {
+        public uint currentID { get; private set; }
+
+        public carType currentType { get; private set; }
+
+        public float currentBalance { get; private set; }
+        
+        public Car(uint id, carType type, float balance)
+        {
+            currentID = id;
+            currentType = type;
+            currentBalance = balance;
+        }
+
+        public float addToBalance(float someMoney)
+        {
+            currentBalance += someMoney;
+            return currentBalance;
+        }
+
+        public float removeFromBalance(float someMoney)
+        {
+            currentBalance -= someMoney;
+            return currentBalance;
+        }
+    }
+    
+}
